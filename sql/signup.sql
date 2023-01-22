f0734b91c397945943a8343ba7848e33a949a5ff
Pawan R. Raut <91772105+Pavan-123-tech@users.noreply.github.com>
2023-01-22 11:48:23 +0530

    Add files via upload

---
+++ sql/signup.sql
@@ -0,0 +1,46 @@
+
+-- Create database 
+-- @Pawan Raut
+-- git 26
+-- Pavan-123-Tech
+-- pavanraut9665@gmail.com
+
+
+Database: `signup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_member`
+--
+
+CREATE TABLE `tbl_member` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-----------
+
+Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_member`
+--
+ALTER TABLE `tbl_member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_member`
+--
+ALTER TABLE `tbl_member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
